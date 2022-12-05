233ca71888b06d658f21a4ead1207e4bec8f6f0a
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-12-05 07:37:34 -0700

    Update advanced_scan.py

--- TCM Security/PNPT/Python/advanced_scan.py
+++ TCM Security/PNPT/Python/advanced_scan.py
@@ -1,69 +1,57 @@
-# python3 advanced_scan.py -t example.com -p 1-1024 -n 10
-
+# Import required libraries
 import socket
-import argparse
 import threading
+import sys
 
-# Define a banner to display when the script is run
+# Define the banner
 banner = """
-  ____  _    _ _____ _   _ _____ ____ _____ _   _ _____ ____  
- / ___|| |  | | ____| \ | | ____|  _ \_   _| | | | ____|  _ \ 
- \___ \| |  | |  _| |  \| |  _| | |_) || | | | | |  _| | | | |
-  ___) | |__| | |___| |\  | |___|  _ < | | | |_| | |___| |_| |
- |____/ \____/|_____|_| \_|_____|_| \_\|_|  \___/|_____|____/ 
-                                                                
+ ______   ______     ______     ______      __  __     __   __     ______     ______     __  __     ______     ______    
+/\  == \ /\  __ \   /\  == \   /\__  _\    /\ \/ /    /\ "-.\ \   /\  __ \   /\  ___\   /\ \/ /    /\  ___\   /\  == \   
+\ \  _-/ \ \ \/\ \  \ \  __<   \/_/\ \/    \ \  _"-.  \ \ \-.  \  \ \ \/\ \  \ \ \____  \ \  _"-.  \ \  __\   \ \  __<   
+ \ \_\    \ \_____\  \ \_\ \_\    \ \_\     \ \_\ \_\  \ \_\\"\_\   \ \_____\  \ \_____\  \ \_\ \_\  \ \_____\  \ \_\ \_\ 
+  \/_/     \/_____/   \/_/ /_/     \/_/      \/_/\/_/   \/_/ \/_/   \/_____/   \/_____/   \/_/\/_/   \/_____/   \/_/ /_/ 
+
+
+Coded by: wayahlife
+https://github.com/wayahlife
 """
 
-# Create a parser object to handle command line arguments
-parser = argparse.ArgumentParser()
-
-# Add an argument for the target host
-parser.add_argument("-t", "--target", help="Target host to scan")
-
-# Add an argument for the range of ports to scan
-parser.add_argument("-p", "--portrange", help="Range of ports to scan (e.g. 1-1024)")
-
-# Add an argument for the number of threads to use
-parser.add_argument("-n", "--numthreads", help="Number of threads to use")
-
-# Add an argument for the timeout duration
-parser.add_argument("-T", "--timeout", help="Timeout duration (in seconds)")
-
-# Parse the command line arguments
-args = parser.parse_args()
-
 # Print the banner
+print("-" * 120)
 print(banner)
+print("-" * 120)
 
-# Prompt the user for the target host, if not specified on the command line
-if args.target is None:
-    host = input("Enter the target host: ")
-else:
-    host = args.target
+# Get the host and port range from the user
+try:
+    host = input("Enter the host address to scan: ")
+    start_port, end_port = map(int, input("Enter the port range to scan (e.g. 1-65535): ").split("-"))
+except (ValueError, KeyboardInterrupt):
+    print("Exiting.")
+    sys.exit()
 
-# Prompt the user for the port range, if not specified on the command line
-if args.portrange is None:
-    port_range = input("Enter the port range (e.g. 1-1024): ")
-else:
-    port_range = args.portrange
+# Define the port scanning function
+def scan_port(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+    result = sock.connect_ex((host, port))
+    if result == 0:
+        try:
+            service = socket.getservbyport(port)
+            print("Port {} is open ({})".format(port, service))
+        except OSError:
+            print("Port {} is open".format(port))
+    sock.close()
 
-# Split the port range into a start and end port
-start_port, end_port = map(int, port_range.split("-"))
+# Scan the ports
+threads = []
+for port in range(start_port, end_port+1):
+    thread = threading.Thread(target=scan_port, args=(host, port))
+    thread.start()
+    threads.append(thread)
 
-# Prompt the user for the number of threads to use, if not specified on the command line
-if args.numthreads is None:
-    num_threads = int(input("Enter the number of threads to use: "))
-else:
-    num_threads = int(args.numthreads)
+# Wait for all threads to complete
+for thread in threads:
+    thread.join()
 
-# Prompt the user for the timeout duration, if not specified on the command line
-if args.timeout is None:
-    timeout = int(input("Enter the timeout duration (in seconds): "))
-else:
-    timeout = int(args.timeout)
-
-# Create a list of ports to scan
-ports = list(range(start_port, end_port + 1))
-
-# Divide the list of ports evenly among the threads
-port_lists = [ports[i::num_threads] for i in range(num_threads)]
+# Print a completion message
+print("Port scan complete.")
