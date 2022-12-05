bd17373c11055dba6ab3edbff76ab936c75d3359
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-12-04 23:00:50 -0700

    Update advanced_scan.py

--- TCM Security/PNPT/Python/advanced_scan.py
+++ TCM Security/PNPT/Python/advanced_scan.py
@@ -1,8 +1,18 @@
+# python3 advanced_scan.py -t example.com -p 1-1024 -n 10
+
 import socket
 import argparse
 import threading
 
-# python3 advanced_scan.py -t example.com -p 1-1024 -n 10
+# Define a banner to display when the script is run
+banner = """
+  ____  _    _ _____ _   _ _____ ____ _____ _   _ _____ ____  
+ / ___|| |  | | ____| \ | | ____|  _ \_   _| | | | ____|  _ \ 
+ \___ \| |  | |  _| |  \| |  _| | |_) || | | | | |  _| | | | |
+  ___) | |__| | |___| |\  | |___|  _ < | | | |_| | |___| |_| |
+ |____/ \____/|_____|_| \_|_____|_| \_\|_|  \___/|_____|____/ 
+                                                                
+"""
 
 # Create a parser object to handle command line arguments
 parser = argparse.ArgumentParser()
@@ -22,52 +32,38 @@ parser.add_argument("-T", "--timeout", help="Timeout duration (in seconds)")
 # Parse the command line arguments
 args = parser.parse_args()
 
-# Extract the target host and port range from the arguments
-host = args.target
-port_range = args.portrange
+# Print the banner
+print(banner)
+
+# Prompt the user for the target host, if not specified on the command line
+if args.target is None:
+    host = input("Enter the target host: ")
+else:
+    host = args.target
+
+# Prompt the user for the port range, if not specified on the command line
+if args.portrange is None:
+    port_range = input("Enter the port range (e.g. 1-1024): ")
+else:
+    port_range = args.portrange
 
 # Split the port range into a start and end port
 start_port, end_port = map(int, port_range.split("-"))
 
-# Extract the number of threads to use
-num_threads = int(args.numthreads)
+# Prompt the user for the number of threads to use, if not specified on the command line
+if args.numthreads is None:
+    num_threads = int(input("Enter the number of threads to use: "))
+else:
+    num_threads = int(args.numthreads)
 
-# Extract the timeout duration
-timeout = int(args.timeout)
+# Prompt the user for the timeout duration, if not specified on the command line
+if args.timeout is None:
+    timeout = int(input("Enter the timeout duration (in seconds): "))
+else:
+    timeout = int(args.timeout)
 
 # Create a list of ports to scan
 ports = list(range(start_port, end_port + 1))
 
 # Divide the list of ports evenly among the threads
 port_lists = [ports[i::num_threads] for i in range(num_threads)]
-
-# Define a function to scan a given list of ports
-def scan_ports(port_list):
-    # Create a socket object
-    s = socket.socket()
-
-    # Set the socket timeout
-    s.settimeout(timeout)
-
-    # Scan the specified list of ports
-    for port in port_list:
-        try:
-            # Try to connect to the target host and port
-            s.connect((host, port))
-            print(f"Port {port} is open on {host}")
-        except:
-            # If the connection fails or times out, print a message indicating that the port is closed
-            print(f"Port {port} is closed on {host}")
-
-# Create a list of threads
-threads = []
-
-# Start a new thread for each list of ports
-for port_list in port_lists:
-    thread = threading.Thread(target=scan_ports, args=(port_list,))
-    thread.start()
-    threads.append(thread)
-
-# Wait for all threads to finish
-for thread in threads:
-    thread.join()
