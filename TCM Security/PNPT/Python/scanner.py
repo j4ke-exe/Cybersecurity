5ac53a528649e1339e6fc9266bad8877f9036ae7
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-12-04 20:06:10 -0700

    Update scanner.py

--- TCM Security/PNPT/Python/scanner.py
+++ TCM Security/PNPT/Python/scanner.py
@@ -1,18 +1,18 @@
 #!/bin/python3
-# Usage: python3 scanner.py <ip>
+#Usage: python3 scanner.py <ip>
 
 import sys
 import socket
 from datetime import datetime as dt
 
-# Define target
-if len(sys.argv) == 2: # argv 1 = "scanner.py", argv 2 = "<ip>"
-    target = socket.gethostbyname(sys.argv[1]) # translate hostname to IPv4
+#Define target
+if len(sys.argv) == 2: #argv 1 = "scanner.py", argv 2 = "<ip>"
+    target = socket.gethostbyname(sys.argv[1]) #translate hostname to IPv4
 else:
     print("Invalid amount of arguments.")
     print("Syntax python3 scanner.py <ip>")
 
-# Banner
+#Banner
 print("-" * 50)
 print("Scanning target: " + target)
 print("Time started: " + str(dt.now()))
@@ -22,7 +22,7 @@ try:
     for port in range(1, 2000):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
-        result = s.connect_ex((target, port)) # error indicator -- if port is open then return print statement
+        result = s.connect_ex((target, port)) #error indicator -- if port is open then return print statement
         if result == 0:
             print(f"Port {port} is open.")
         s.close()
